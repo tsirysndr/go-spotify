b95909c976d969e83fcf656b28ce575aa8003237
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2019-12-31 21:59:40 +0300

    :art: Improving examples structure

---
+++ example/get-user-profile/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/tsirysndr/go-spotify"
+)
+
+func main() {
+	// token
+	token := "BQDid21lB1igrlfPLB5pmWWr31BSeeKgJU8YJf1lQju6MhJLlzn097bgULQA3ATzEzhHlgzHjGCHxn9lKpIuBsO907agI9r8Bzka0OQtn1DlMN5Y-cuZPEuNQwNgxE1rEjLomYVw6fE5NmTBq4bVegeFnM3pGVqcyQ"
+	client := spotify.NewClient(token)
+	// User id
+	id := "lyp9kipowzwqehy4ppj5owl7p"
+	res, _ := client.User.UserProfile(id)
+	user, _ := json.Marshal(res)
+	fmt.Println(string(user))
+}
