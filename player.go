1ac0d522f5d7207573da56087ba035b0d3a51154
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2020-01-03 11:04:40 +0300

    :sparkles: Get the Current User's Recently

--- player.go
+++ player.go
@@ -12,6 +12,21 @@ type PlayingTrack struct {
 	IsPlaying            bool     `json:"is_playing,omitempty"`
 }
 
+type RecentlyPlayedTracks struct {
+	Items []struct {
+		Track    *Track   `json:"track,omitempty"`
+		PlayedAt string   `json:"played_at,omitempty"`
+		Context  *Context `json:"context,omitempty"`
+	} `json:"items,omitempty"`
+	Next    string `json:"next,omitempty"`
+	Cursors *struct {
+		After  string `json:"after,omitempty"`
+		Before string `json:"before,omitempty"`
+	} `json:"cursors,omitempty"`
+	Limit int    `json:"limit,omitempty"`
+	Href  string `json:"href,omitmepty"`
+}
+
 type Context struct {
 	ExternalURLs ExternalURLs `json:"external_urls,omitempty"`
 	Href         string       `json:"href,omitempty"`
@@ -25,9 +40,27 @@ type Actions struct {
 	} `json:"disallows,omitempty"`
 }
 
+type RecentlyPlayedTracksParams struct {
+	Type  string `url:"type"`
+	Limit int    `url:"type"`
+	After int    `url:"after"`
+}
+
 func (s *PlayerService) GetCurrentlyPlaying() (*PlayingTrack, error) {
 	var err error
 	res := new(PlayingTrack)
 	s.client.base.Path("me/player/").Get("currently-playing").Receive(res, err)
+	return res, err
+}
+
+func (s *PlayerService) GetRecentlyPlayed(filter string, limit, after int) (*RecentlyPlayedTracks, error) {
+	var err error
+	params := &RecentlyPlayedTracksParams{
+		filter,
+		limit,
+		after,
+	}
+	res := new(RecentlyPlayedTracks)
+	s.client.base.Path("me/player/").Get("recently-played").QueryStruct(params).Receive(res, err)
 	return res, err
 }
